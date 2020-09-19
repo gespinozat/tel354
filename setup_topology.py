5346c5c360cd1b1de7f18115ac5465d4ab9e7bf5
German Espinoza <g.espinozat@pucp.pe>
2020-09-18 21:10:28 -0500

    Preliminar

---
+++ setup_topology.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+import csv
+import paramiko
+
+lines = []
+
+with open('SDN_VMs.csv', 'r') as file:
+    reader = csv.reader(file)
+    for row in reader:
+        lines.append(row)
+
+for row in lines:
+    if row[3] == 'TEL354_ovs':
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(hostname=row[2], username='ubuntu',password='ubuntu')
+        print "Configurando Host/Switch, IP: "+ row[2]
+        stdin, stdout, stderr = ssh.exec_command("echo 'Este es un host u OVS' > test.txt")
+        # stdin, stdout, stderr = ssh.exec_command("git clone https://github.com/gespinozat/tel354.git")
+        # stdin, stdout, stderr = ssh.exec_command("cd tel354 && ./setup_node.sh")
+        ssh.close()
+        
+    else:
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(hostname=row[2], username='ubuntu',password='ubuntu')
+        print "Configurando Controlador, IP: "+ row[2]
+        stdin, stdout, stderr = ssh.exec_command("echo 'Este es un controlador' > test.txt")
+        # stdin, stdout, stderr = ssh.exec_command("git clone https://github.com/gespinozat/tel354.git")
+        # stdin, stdout, stderr = ssh.exec_command("cd tel354 && ./setup_controller.sh")
+        # stdin, stdout, stderr = ssh.exec_command("cd tel354 && ./run_floodlight.sh")
+        ssh.close()
